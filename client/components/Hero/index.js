2a4c7edc6746542bfdd1d75bf85e5dbdcbec1584
Travis Henson <travisghenson@gmail.com>
2020-01-27 23:13:16 -0600

    Updated home page hero with placeholder

--- client/components/Hero/index.js
+++ client/components/Hero/index.js
@@ -4,9 +4,11 @@ import './style.scss';
 function Hero() {
   return (
     <div className='hero'>
-      <h1>Welcome to TwitchReact!</h1>
-      <p>TwitchReact is a Twitch client that is built using the Twitch API and the Internet Games Database API.</p>
-      <p>It is currently under construction with limited functionality, but feel free to look around.</p>
+      <div className='heroContent'>
+        <h1>Welcome to TwitchReact!</h1>
+        <p>TwitchReact is a Twitch client that is built using the Twitch API and the Internet Games Database API.</p>
+        <p>It is currently under construction with limited functionality, but feel free to look around.</p>
+      </div>
     </div>
   )
 }
