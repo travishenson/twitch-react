668321dd260b1dec615a2235d7ecce3ae4cd89f0
Travis Henson <travisghenson@gmail.com>
2020-01-27 20:39:44 -0600

    Updated Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      filename: './dist/index.html',
+      filename: './index.html',
       template: './utils/index.html'
     })
   ]
