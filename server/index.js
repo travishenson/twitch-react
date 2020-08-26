5a32990987efc5882119d31d9ff88f8c8287b751
travishenson <hello@travishenson.com>
2020-08-26 00:12:20 -0800

    Attempting production error fix

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Serve static React files
-app.use(express.static(path.join(__dirname, '../public/')));
+app.use(express.static(path.join(__dirname, '../public')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('/*', function (request, response){
+    response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+  });
+};
 
 // Twitch API setup and routes
 const helixGet = async (endpoint) => {
